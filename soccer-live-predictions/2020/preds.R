ff18eff8d40c7391ad787ca7ff649e984c74403a
luizfgnmaia <luiz_fgnm@hotmail.com>
2021-03-18 20:14:25 -0300

    (t+1)^2

--- soccer-live-predictions/2020/preds.R
+++ soccer-live-predictions/2020/preds.R
@@ -10,4 +10,4 @@ pred_mod_1(home_team = "Flamengo", away_team = "Vasco da Gama")
 
 pred_mod_2(home_team = "Flamengo", away_team = "Vasco da Gama")
 
-pred_mod_3(home_team = "Flamengo", away_team = "Vasco da Gama")
+pred_mod_3(away_team = "Flamengo", home_team = "Internacional")
