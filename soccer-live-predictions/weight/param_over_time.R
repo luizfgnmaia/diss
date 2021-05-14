eea983b89f00d525698e92409ba66200ebdfe855
luizfgnmaia <luiz_fgnm@hotmail.com>
2021-05-13 22:05:14 -0300

    rbb

--- soccer-live-predictions/weight/param_over_time.R
+++ soccer-live-predictions/weight/param_over_time.R
@@ -7,7 +7,7 @@ load("weight/data/mod_8_dc.RData")
 y = NULL
 x = as.Date(names(mod_8_dc), format = "%Y-%m-%d")
 for(i in 1:length(mod_8_dc)) {
-  y[i] = exp(mod_8_dc[[i]]$omega_xy["behind"])
+  y[i] = mod_8_dc[[i]]$alpha["Red Bull Bragantino"]
 }
 
 tibble(x = x, y = y) %>%
@@ -15,3 +15,9 @@ tibble(x = x, y = y) %>%
   geom_line() +
   theme_bw() +
   geom_point()
+
+names(y) = x
+
+which(y == max(y))
+# 2019-05-01 
+# 313 
